17e2df92cc1716a866daaefef0bfa6b7f504c9e7
yanninho <y4nn5m@gmail.com>
2015-09-09 20:45:59 +0200

    accept headers happyrest

--- sources/server.js
+++ sources/server.js
@@ -27,9 +27,10 @@ module.exports = function(app, config) {
 
 		app.use(function(req, res, next){
 			res.header('Access-Control-Allow-Credentials', true);
-			res.header('Access-Control-Allow-Origin', req.headers.origin);
+			res.header('Access-Control-Allow-Origin', '*');
 			res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-			res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Cache-Control');
+			res.header('Access-Control-Allow-Headers', '*');
+			res.header('Access-Control-Expose-Headers', 'content-range, accept-range, link');	
 			res.header('content-type', 'application/json; charset=utf-8');
 			if (req.method === 'OPTIONS') {
 				res.statusCode = 204;
