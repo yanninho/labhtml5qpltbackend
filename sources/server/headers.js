6300707850694468aa6b99abd42a22143d91a829
yanninho <y4nn5m@gmail.com>
2015-09-14 00:46:05 +0200

    expose headers

--- sources/server/headers.js
+++ sources/server/headers.js
@@ -7,6 +7,7 @@
   app.use(cors());
 
   app.use(function(req, res, next){
+    res.header('Access-Control-Expose-Headers', 'content-range, accept-range, link');
     if (req.method === 'OPTIONS') {
       res.statusCode = 204;
       return res.end();
